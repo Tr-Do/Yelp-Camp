7a1db5ec01628e5f3c7e3f839324b2babb456674
T Do <gofoyo@hotmail.com>
2025-06-03 23:57:16 -0500

    add review edit button

--- public/javascripts/review.js
+++ public/javascripts/review.js
@@ -1,16 +1,11 @@
-const { connect } = require("mongoose");
-
 function editReview(rev) {
-    const doc = document.getElementById('editt');
-    doc.classList.add('d-none');
-    const content = document.createElement('textarea');
-    content.value = rev;
-    const doc2 = document.getElementById('savee');
-    doc2.classList.remove('d-none');
-    doc.parentNode.insertBefore(content, doc.nextSibling);
+    document.getElementById('savee').classList.remove('d-none');
+    document.getElementById('editedReview').classList.remove('d-none');
+    document.getElementById('editt').classList.add('d-none');
 }
 
 function savee(rev) {
     document.getElementById('editt').classList.remove('d-none');
     document.getElementById('savee').classList.add('d-none');
+    document.getElementById('editedReview').classList.add('d-none');
 }
