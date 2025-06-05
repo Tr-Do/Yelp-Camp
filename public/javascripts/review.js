9942d54400413f402db148daf97a088d968788f1
T Do <tdo02@jaguar.tamu.edu>
2025-06-04 23:49:56 -0500

    remove redundant review textarea

--- public/javascripts/review.js
+++ public/javascripts/review.js
@@ -1,11 +1,10 @@
-function editReview(rev) {
-    document.getElementById('savee').classList.remove('d-none');
-    document.getElementById('editedReview').classList.remove('d-none');
-    document.getElementById('editt').classList.add('d-none');
+function editReview(id) {
+    document.getElementById(savee-${id}).classList.remove('d-none');
+    // document.getElementById(editedReview-${id}).classList.remove('d-none');
+    document.getElementById(editt-${id}).classList.add('d-none');
 }
-
 function savee(rev) {
     document.getElementById('editt').classList.remove('d-none');
-    document.getElementById('savee').classList.add('d-none');
-    document.getElementById('editedReview').classList.add('d-none');
+    // document.getElementById('savee-${id}').classList.add('d-none');
+    document.getElementById(editedReview-${id}).classList.add('d-none');
 }
